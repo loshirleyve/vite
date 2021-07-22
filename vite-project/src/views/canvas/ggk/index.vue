bf94a2304c2590458e8c817d1ee05e104c1384cc
liuli <1242663098@qq.com>
2021-07-22 08:24:06 +0800

    canvas ggk

---
+++ vite-project/src/views/canvas/ggk/index.vue
@@ -0,0 +1,105 @@
+<!--
+ * @Author: liuli
+ * @Date: 2021-07-22 07:01:40
+ * @LastEditTime: 2021-07-22 08:20:36
+ * @LastEditors: Please set LastEditors
+ * @Description: 
+ * @FilePath: /vite/vite-project/src/views/canvas/ggk/index.vue
+-->
+<template>
+  <div class="canvas-wrapper">
+    <div id="ggk" ref="ggk">
+      <div id="jp">{{ bonus || '谢谢惠顾' }}</div>
+      <canvas id="canvas" ref="canvas"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CanvasIndex',
+  data () {
+    return {
+      bonus: ''
+    }
+  },
+  mounted() {
+    const canvas = this.$refs.canvas
+    const ggkDom = this.$refs.ggk
+    const ctx = canvas.getContext('2d')
+    // 第一个矩形为目标图像
+    ctx.fillStyle = 'darkgrey'
+    ctx.fillRect(0, 0, 400, 200)
+    
+    ctx.font = '20px'
+    ctx.fillStyle = '#fff'
+    ctx.fillText('刮刮卡', 140, 80)
+    
+    // 第二个矩形为源图像
+    ctx.fillStyle = 'deepskyblue'
+    ctx.fillRect(400, 200, 180, 200)
+
+    // 是否允许绘制
+    let isDraw = false
+    canvas.ontouchstart = () => {
+      isDraw = true
+      console.log(isDraw)
+    }
+    // 移动的时候绘制圆形，将源图像内的目标的内容清除掉
+    canvas.ontouchmove = (e) => {
+      if (isDraw) {
+        const touch = e.touches[0]
+        // 当前点击的页面位置 - canvas 容器的偏移量 = 当前点击位置在 canvas 中的相对位置
+        const x = touch.clientX - ggkDom.offsetLeft
+        const y = touch.clientY - ggkDom.offsetTop
+        ctx.globalCompositeOperation = 'destination-out'
+        ctx.arc(x, y, 20, 0, 2 * Math.PI)
+        ctx.fill()
+      }
+    }
+    canvas.ontouchend = () => {
+      isDraw = false
+      console.log(isDraw)
+    }
+
+    const arr = [{content: '一等奖', p: 0.1}, {content: '二等奖', p: 0.2}, {content: '三等奖', p: 0.6}, ]
+    const ran = Math.random()
+    if (ran < arr[0].p) {
+      this.bonus = arr[0].content
+    } else if (ran < arr[0].p + arr[1].p) {
+      this.bonus = arr[1].content
+    } else if (ran < arr[0].p + arr[1].p + arr[2].p) {
+      this.bonus = arr[2].content
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+#ggk {
+  width: 100vw;
+  height: 200px;
+  overflow: hidden;
+}
+#jp {
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  color: pink;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  font-size: 32px;
+}
+#canvas {
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  overflow: hidden;
+}
+</style>
---
+++ vite-project/src/views/canvas/index.vue
@@ -0,0 +1,77 @@
+<!--
+ * @Author: liuli
+ * @Date: 2021-07-22 07:01:40
+ * @LastEditTime: 2021-07-22 07:46:51
+ * @LastEditors: Please set LastEditors
+ * @Description: 
+ * @FilePath: /vite/vite-project/src/views/canvas/index.vue
+-->
+<template>
+  <div class="canvas-wrapper">
+    <p>canvas</p>
+    <canvas id="canvas" ref="canvas" width="800" height="600"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CanvasIndex',
+  data () {
+    return {
+      
+    }
+  },
+  mounted() {
+    const canvas = this.$refs.canvas
+    const ctx = canvas.getContext('2d')
+    // 第一个矩形为目标图像
+    ctx.fillStyle = 'hotpink'
+    ctx.fillRect(100, 100, 200, 200)
+
+    /**
+     * 默认为 globalCompositeOperation:source-over, 源图像叠加在目标图像上
+     * console.log(ctx)
+     * 
+     * 在目标图像顶部显示源图像，源图像位于目标图像之外的部分是不可见的
+     * ctx.globalCompositeOperation = 'source-atop'
+     * 
+     * 在目标图像中显示源图像。只有目标图像之内的源图像部分会显示，目标图像是透明的。
+     * ctx.globalCompositeOperation = 'source-in'
+     * 
+     * 在目标图像之外显示源图像。只有目标图像之外的源像部分会显示，目标图像是透明的。
+     * ctx.globalCompositeOperation = 'source-out'
+     * 
+     * 在源图像上显示目标图像。
+     * ctx.globalCompositeOperation = 'destination-over'
+     * 
+     * 在源图像顶部显示目标图像。目标图像位于源图像之外的部分是不可见的。
+     * ctx.globalCompositeOperation = 'destination-atop'
+     * 
+     * 在源图像中显示，目标图像只有源图像之内的目标图像部分会被显示，源图相是透明的。
+     * ctx.globalCompositeOperation = 'destination-in'
+     * 
+     * 在源图像之外显示。目标图像只有源图像之外的目标图像部分会被显示，源图相是透明的。
+     * ctx.globalCompositeOperation = 'destination-out'
+     * 
+     * 显示原图像+目标图像。
+     * ctx.globalCompositeOperation = 'lighter'
+     * 
+     * 显示源图像，忽略目标图像。
+     * ctx.globalCompositeOperation = 'copy'
+
+     * xor 等同于 lighter, 使用异或操作对源图像与目标图像进行组合。
+     * ctx.globalCompositeOperation = 'xor'
+     */
+    
+    ctx.globalCompositeOperation = 'destination-out'
+
+    // 第二个矩形为源图像
+    ctx.fillStyle = 'deepskyblue'
+    ctx.fillRect(200, 200, 200, 200)
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
